3531224b69aa14c248cb331ff7542c6bf50ff45f
hekkort <harryvanderknaap@gmail.com>
2025-07-03 14:51:08 +0200

    added build.sh and basepath

--- src/main.py
+++ src/main.py
@@ -1,20 +1,24 @@
 import os
 import shutil
 from splitblocks import *
-from pathlib import Path
+import sys
 
 def main():
     root = "/home/hekkort/workspace/github.com/hekkort/ozmor"
-    from_path = root + "/content/index.md"
     template_path = root + "/template.html"
     dest_path = root + "/public"
     src = root + "/static"
     content = root + "/content"
 
+    basepath = sys.argv[0]
+    if basepath == "":
+        basepath = "/"
 
     copy(src, dest_path)
 
-    generate_pages_recursive(content, template_path, dest_path)
+    generate_pages_recursive(content, template_path, dest_path, basepath)
+
+
 
 
 
@@ -42,7 +46,7 @@ def extract_title(markdown):
                 return block.strip()
     raise Exception("no h1 header found")
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
     md_contents = ""
     html_template = ""
@@ -57,22 +61,24 @@ def generate_page(from_path, template_path, dest_path):
     title = extract_title(md_contents)
     html_template = html_template.replace("{{ Title }}", title)
     html_template = html_template.replace("{{ Content }}", html_string)
+    html_template = html_template.replace('href="/', f'href="{basepath}')
+    html_template = html_template.replace('src="/', f'src="{basepath}')
 
     with open(dest_path + "/index.html", "w") as file:
         file.write(html_template)
 
     
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath):
     items = os.listdir(dir_path_content)
 
     for item in items:
         if item.endswith(".md"):
             print(f"src path: {os.path.join(dir_path_content, item)}")
-            generate_page(os.path.join(dir_path_content, item), template_path, dest_dir_path)
+            generate_page(os.path.join(dir_path_content, item), template_path, dest_dir_path, basepath)
         elif os.path.isdir(os.path.join(dir_path_content, item)):
             os.mkdir(os.path.join(dest_dir_path, item))
             print(f"dst path: {os.path.join(dest_dir_path, item)}")
-            generate_pages_recursive(os.path.join(dir_path_content, item), template_path, os.path.join(dest_dir_path, item))
+            generate_pages_recursive(os.path.join(dir_path_content, item), template_path, os.path.join(dest_dir_path, item), basepath)
             
             
 
