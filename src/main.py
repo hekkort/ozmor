a24510e75f66149b4755bc8e8d046bbec16bf4ef
hekkort <harryvanderknaap@gmail.com>
2025-07-03 11:38:25 +0200

    added copy from static to public

--- src/main.py
+++ src/main.py
@@ -1,9 +1,30 @@
-from textnode import TextNode
 import os
+import shutil
+from splitblocks import *
 
 def main():
-    copy()
+    src = "/home/hekkort/workspace/github.com/hekkort/ozmor/static"
+    dst = "/home/hekkort/workspace/github.com/hekkort/ozmor/public"
+    copy(src, dst)
 
-def copy():
-    print(os.listdir(".."))
+
+def copy(src, dst):
+    if os.path.exists(dst):
+        shutil.rmtree(dst)
+    os.mkdir(dst)
+
+    items = os.listdir(src)
+    for item in items:
+        if os.path.isfile(os.path.join(src, item)):
+            print(f"Copying file: {os.path.join(src, item)}")
+            shutil.copy(os.path.join(src, item), dst)
+        elif os.path.isdir(os.path.join(src, item)):
+            print(f"Copying directory: {os.path.join(src, item)}")
+            os.mkdir(os.path.join(dst, item))
+            copy(os.path.join(src, item), os.path.join(dst, item))
+
+def extract_title(markdown):
+    pass
+    
+
 main()
