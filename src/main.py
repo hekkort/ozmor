433110e9941a99ca065091c32b88b337e2131bcc
hekkort <harryvanderknaap@gmail.com>
2025-07-03 20:09:41 +0200

    also added href= vs the quotes version

--- src/main.py
+++ src/main.py
@@ -60,6 +60,7 @@ def generate_page(from_path, template_path, dest_path, basepath):
     html_template = html_template.replace("{{ Title }}", title)
     html_template = html_template.replace("{{ Content }}", html_string)
     html_template = html_template.replace('href="/', f'href="{basepath}')
+    html_template = html_template.replace('href=/', f'href={basepath}')
     html_template = html_template.replace('src="/', f'src="{basepath}')
     html_template = html_template.replace('src=/', f'src={basepath}')
 
